ff02dc6a551b69153c37ba204dc99d33b5a77a31
Iamthecoderofgods <22mjames@strichardgwyn.school>
2025-09-02 18:56:24 +0100

    Add files via upload

--- flappy_bird.py
+++ flappy_bird.py
@@ -10,6 +10,9 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.update()
 ground_scroll = -100
 scroll_speed = 0.25
+flying = False
+game_over = False
+
 
 Backround = pygame.image.load("images/flappy_backround.png")
 ground = pygame.image.load("images/flapply_ground.png")
@@ -31,7 +34,25 @@ class Bird(pygame.sprite.Sprite):
         self.clicked = False
         self.velocity = 0
         print("doing creation")
+    def update(self):
+        if flying == True:
+            self.velocity += 0.7
+            if self.velocity > 10000000:
+                self.velocity = 10000000
+            if self.rect.bottom < 850:
+                self.rect.y += (int(self.velocity))
+        if game_over == False:
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.velocity -=150
+            if self.velocity > 150:
+                self.velocity = 8
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+        
+            
 Birdgroup = pygame.sprite.Group()
+
 flappy = Bird(400,400)
 
 Birdgroup.add(flappy)
@@ -41,9 +62,12 @@ while Running:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             Running = False
+        if e.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+            flying = True
     screen.blit(Backround_image1,(0,0))
     screen.blit(ground,(ground_scroll,800))
     Birdgroup.draw(screen)
+    Birdgroup.update()
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 50:
         ground_scroll = 0
