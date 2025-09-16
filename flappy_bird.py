710122fc7f45802138a093bf478c5ab6a3171130
Iamthecoderofgods <22mjames@strichardgwyn.school>
2025-09-16 18:58:46 +0100

    Add files via upload

--- flappy_bird.py
+++ flappy_bird.py
@@ -12,6 +12,9 @@ ground_scroll = -100
 scroll_speed = 0.25
 flying = False
 game_over = False
+pipegap = 150
+pipe_frequency = 1500 
+last_pipe = pygame.time.get_ticks()-pipe_frequency
 
 
 Backround = pygame.image.load("images/flappy_backround.png")
@@ -19,6 +22,21 @@ ground = pygame.image.load("images/flapply_ground.png")
 pipes = pygame.image.load("images/Flappy_pipe.png")
 #flappy2 = pygame.image.load("image/Flappy_bird1.png")
 Backround_image1 = pygame.transform.scale(Backround,(900,850))
+
+class pipe(pygame.sprite.Sprite):
+    def __init__(self, x,y,position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/Flappy_pipe.png")
+        self.Rect = self.image.get_rect()
+        if position == 1:#top pipe
+            self.image = pygame.transform.flip(self.image,False,True)
+            self.Rect.bottomleft = [x,y-int(pipegap/2)]
+        elif position == -1:
+            self.Rect.topleft = [x,y+int(pipegap/2)]
+            
+
+
+
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x,y):
@@ -56,14 +74,18 @@ class Bird(pygame.sprite.Sprite):
             if self.counter > flap_cool_down:
                 self.counter = 0
                 self.index +=1
-                if self.index > len(self.flappy_images):
+                print(self.flappy_images)
+                print(self.counter)
+                if self.index >= len(self.flappy_images):
                     self.index = 0
                 self.image = self.flappy_images[self.index]
 
-        
-            
-Birdgroup = pygame.sprite.Group()
+            self.image = pygame.transform.rotate(self.flappy_images[self.index],self.velocity*-2)
+        else:
+            self.image = pygame.transform.rotate(self.flappy_images[self.index],-90)
 
+Birdgroup = pygame.sprite.Group()
+pipegroup = pygame.sprite.Group()
 flappy = Bird(400,400)
 
 Birdgroup.add(flappy)
@@ -75,10 +97,16 @@ while Running:
             Running = False
         if e.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
             flying = True
+    if flying == True and game_over == False:
+        time_now = pygame.time.get_ticks()
+        if time_now - last_pipe > pipe_frequency:
+            bottom_pipe = pipe()
     screen.blit(Backround_image1,(0,0))
     screen.blit(ground,(ground_scroll,800))
     Birdgroup.draw(screen)
     Birdgroup.update()
+    pipegroup.draw(screen)
+    pipegroup.update()
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 50:
         ground_scroll = 0
